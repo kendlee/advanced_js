b8603ad5a7ed89c0f32e1bae9b31c03e33688efb
Ken Lee <ken.lee@nokia.com>
2016-06-28 17:36:30 +0800

    Committed code.

--- myscript.js
+++ myscript.js
@@ -1,13 +1,13 @@
 'use strict';
 (function () {
-    var a = 'hi';
-    function greet () {
-        console.log(a, "// in greet 1" );
-        var a = 'yay';
-        console.log(a, "// in greet 2" );
+    console.log(fxn);
+    var fxn = "i'm a fake function";
+
+    console.log(fxn);
+
+    function fxn() {
+        console.log("hi");
     }
-    greet();
-    console.log(a, "// out of greet");
 })();
 
 /**
