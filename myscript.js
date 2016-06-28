20c10d88038e43f5c33bc0b9cdef08be8cff54bc
Ken Lee <ken.lee@nokia.com>
2016-06-28 16:02:15 +0800

    Hoisting intro.

--- myscript.js
+++ myscript.js
@@ -1,26 +1,10 @@
 'use strict';
 (function () {
-    var el = new DisplayElement();
-    el.showOutput('test!');
-    el['showOutput']('another test! :)');
-
-    function DisplayElement () {
-        var obj = {};
-        obj.showOutput = showOutput;
-        obj.clearOutput = clearOutput;
-        obj.getDisplayElement = getDisplayElement;
-        return obj;
-
-        function showOutput(text) {
-            getDisplayElement().innerHTML += text + '<br/>';
-        }
-
-        function clearOutput() {
-            getDisplayElement().innerHTML = '';
-        }
-
-        function getDisplayElement() {
-            return document.querySelector("#js-data");
-        }
+    var a = 'hi';
+    function greet () {
+        console.log(a, "// in greet" );
+        var a;
     }
+    console.log(a, "// out of greet");
+    greet();
 })();
